b2b444f26a749daf0603d9fc044f3d448555df9a
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-09-29 21:38:56 +0530

    quiz app

--- Quiz-App/index.js
+++ Quiz-App/index.js
@@ -20,14 +20,14 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-let currentQuestioIndex=0;
+let currentQuestionIndex=0;
 let score=0;
 
 function startQuiz(){
-    currentQuestioIndex =0;
+    currentQuestionIndex =0;
     score=0;
     nextButton.innerHTML="Next";
     showQuestion();
@@ -35,7 +35,39 @@ function startQuiz(){
 }
 
 function showQuestion(){
-    let currentQuestion = questions[currentQuestioIndex];
-    let questionNo = currentQuestioIndex +1;
-    questionElement.innerHTML= questionNo+ ". "+currentQuestion.question
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML= questionNo+ ". "+currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button")
+        button.innerHTML= answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct =answer.correct;
+        }
+        button.addEventListener("click",selectAnswer)
+    });
 }
+
+function resetState(){
+    nextButton.style.display="none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+}
+
+
+startQuiz();
