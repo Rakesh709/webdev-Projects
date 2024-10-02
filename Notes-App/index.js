ac113eedb7c9014f34e45c9705eeb8a0c9784f45
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-10-02 19:30:28 +0530

    Note App

--- Notes-App/index.js
+++ Notes-App/index.js
@@ -1,1 +1,47 @@
 
+const noteContainer = document.querySelector('.notes-container')
+const createBtn = document.querySelector('.btn')
+
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    noteContainer.innerHTML= localStorage.getItem("notes")
+}
+
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",noteContainer.innerHTML);
+}
+
+
+
+createBtn.addEventListener("click",()=>{
+    let inputBox= document.createElement("p");
+    let img = document.createElement("img")
+    inputBox.className="input-box"
+    inputBox.setAttribute("contenteditable","true")
+    img.src="images/delete.png";
+    noteContainer.appendChild(inputBox).appendChild(img)
+})
+
+noteContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage()
+    }else if(e.target.tagName==="P"){
+        notes= document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup=function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown",event=>{
+    if(event.key ==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
